4c694f9d794890ad0a9b349f07ce7f47b63a31e2
Douer <31438843@qq.com>
2020-12-18 20:50:44 +0800

    R

---
+++ R/test_proteus.R
@@ -0,0 +1,15 @@
+#! /usr/bin/R
+
+library(proteus)
+evi <- read.csv("D:/dataset/R downstream analysis/proteus/ourdata/evidence.csv")
+meta <- read.csv("D:/dataset/R downstream analysis/proteus/ourdata/metadata.csv")
+setwd("D:/dataset/R downstream analysis/proteus/ourdata/")
+pepdat <- makePeptideTable(evi, meta)
+prodat <- makeProteinTable(pepdat)
+prodat.med <- normalizeData(prodat)
+
+# PX from the meta's condition, and you can only compare two conditions once.
+res <- limmaDE(prodat.med, conditions=c("P1","P2"))
+
+# draw volcano plot
+plotVolcano(res)
