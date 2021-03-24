5170183935b7cf7ea0e51055c392bc7e60f1d9ee
ypriverol <ypriverol@gmail.com>
2021-03-24 11:11:06 +0000

    EA output.

--- build.sh
+++ build.sh
@@ -4,16 +4,14 @@
 conda remove --name pquant-conda --all
 
 # build an R environment with base R
-conda env create --quiet -f environment.yml && conda clean -a
+mamba env create --file environment.yml && conda clean -a -y
 
 # activate the environment
 conda activate pquant-conda
 
 # Active R environment.
-R -e "if (!requireNamespace('BiocManager', quietly = TRUE)) install.packages('BiocManager')"
+R -e "install.packages('BiocManager', repos = http://cran.us.r-project.org')"
 R -e "BiocManager::install()"
-R -e "BiocManager::install('limma')"
-R -e "install.packages('ggplot2')"
 R -e "devtools::install_github('bartongroup/proteusLabelFree')"
 R -e "devtools::install_github('bartongroup/proteusTMT')"
 R -e "devtools::install_github('bartongroup/proteusSILAC')"
