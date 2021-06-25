c35036af5d2298c415e82e76ba0112538cdc53c4
Jingwen Deng <46063443+Douerww@users.noreply.github.com>
2021-06-25 10:23:45 +0800

    Update and rename pquant.R to RStudio_pquant.R

--- pquant/pquant.R
+++
@@ -1,9 +0,0 @@
-library('MSstats', warn.conflicts = F, quietly = T, verbose = F)
-
-setwd('D:/dataset/R downstream analysis/pquant/data')
-load("preShiny.RData")
-
-setwd('D:/dataset/R downstream analysis/pquant/')
-source("shiny-app/app.R")
-
-pquant_shiny(DDA2009.proposed, DDA2009.TMP, DDA2009.comparisons)
